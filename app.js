1a805958cc1e2efb18cc3bec446946e5aa81d2f9
Jonish-sm <jonishsubba@gmail.com>
2023-06-29 18:50:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const accessKey = "tEmFgPCNneyycZkgi6A6Rn34rWH2rLNvIz3sky8yeWE";
+
+
+const formEl = document.querySelector("form");
+const inputEl =document.getElementById("search-input");
+const searchResults =document.querySelector(".search-results");
+
+const showMore = document.getElementById("showmore");
+
+let inputData="";
+let page = 1;
+
+async function searchImages(){
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    const results = data.results;
+    console.log(results)
+
+    if(page === 1){
+    searchResults.innerHTML="";
+    }
+    results.map((result)=>{
+        const imageWraper = document.createElement("div")
+        imageWraper.classList.add('search-result');
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description
+        const imageLink = document.createElement('a')
+        imageLink.href =result.links.html;
+        imageLink.target = "_blank"
+        imageLink.textContent = result.alt_description;
+
+
+        
+
+
+        imageWraper.appendChild(image)
+        imageWraper.appendChild(imageLink)
+        searchResults.appendChild(imageWraper)
+    })
+    page++
+    if(page>1){
+        showMore.style.display ="block";
+    }
+};
+
+
+
+formEl.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+
+showMore.addEventListener("click", ()=>{
+    searchImages();
+})
+
+
+
